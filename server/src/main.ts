6d04061dc469fd81b557793e8ead09e5ba6ca62d
ivan-dev <ivanallen64@gmail.com>
2026-08-06 09:44:59 +0800

    mend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    console.log(`Server is running on port ${3000}`,)
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
